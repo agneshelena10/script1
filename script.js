d38afefec906f443747c5dad9ae5e817a0f1e6db
Agnes Helena <agneshelena11@gmail.com>
2019-02-12 11:52:35 -0200

    adicionando script

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let valores = [15, 10, 30, 20];
+for (let valor of valores ) {
+
+if (valor % 2 == 0)  {
+    console.log('valor par');
+}
+else {
+    console.log('valor ímpar');
+}
+console.log(valor);
+}
