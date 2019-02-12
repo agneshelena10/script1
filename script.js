0490db914388d30c121fb6ac9a81e1df8df8e100
Agnes Helena <agneshelena11@gmail.com>
2019-02-12 12:05:52 -0200

    Adicionando script

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ console.log(valor);
 }
 let objeto = {}; /* objeto usa palavras ao invés de índices*/ 
 /* não é possível fazer laço com objetos */ 
+
+let joaozinho = {
+    noem: "João", 
+    sobrenome: "da Silva", 
+    idade: 17, 
+    namorada: undefined,
+    cachorros: [
+        {nome: "alibaba"}
+        {nome: "gogozinho"}
+    ]
+}
